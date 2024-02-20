e6ad753a54b5c3eea1d655769bbeb9807e5d5f1f
Diego Robles Rojas <125101098+Castle373@users.noreply.github.com>
2024-02-20 03:55:14 -0700

    Basico
    Sirve hasta la sipnosis

---
+++ app/src/main/java/com/diego/popcornapi/Pelicula.kt
@@ -0,0 +1,6 @@
+package com.diego.popcornapi
+
+data class Pelicula(var titulo:String,
+                    var image:Int,
+                    var header:Int,
+                    var sipnosis:String)
